034afa1a55d653d7b6e5c6e738b2a3c69f521cdc
Gabriel M <gmoreno9109@gmail.com>
2023-10-27 00:22:32 +0200

    mis en page

--- script.js
+++ script.js
@@ -1,122 +1,79 @@
 "use strict";
 
 const groBtn = document.querySelector("button");
-const info = document.querySelector(".meal");
-const ingredients = document.querySelector(".grosBordel")
+const grid = document.querySelector(".grid");
 
-groBtn.addEventListener("click", () => {
-  // alert(1)
-  groBtn.classList.remove("relief");
-});
 
-const req = async () => {
+const request = async () => {
   const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-  console.log(res);
   const data = await res.json();
-  console.log(data);
   return data;
 };
-// req()
-//   .then((res) => {
-//     return Object.entries(res.meals[0]).filter(
-//       (e) => e[0].includes("strIngredient") || e[0].includes("strMeasure")
-//     );
-//   })
-//   .then((data) =>
-//     data.forEach((e) => {
-//       console.log(e[1]);
-//     })
-//   );
 
-const hein = () => {
-    let arr1 = []
-    let arr2 = []
-    let finalArr = []
-    req()
-  .then((res) => {
-    arr1 = Object.entries(res.meals[0]).filter((e) => e[0].includes("strIngredient"));
-    arr2 = Object.entries(res.meals[0]).filter((e) => e[0].includes("strMeasure"));
-    for(let i = 0; i < arr1.length; i++){
-        finalArr.push([arr1[i][1], arr2[i][1]])
-    }
-    // console.log(finalArr.filter(e=> e[0]));
-    finalArr = finalArr.filter(e=> e[0])
-    //   finalArr.forEach(e => console.log())
-      for(let i = 0; i < finalArr.length; i++){
-        ingredients.insertAdjacentHTML("afterbegin", `${finalArr[i][0]} : ${finalArr[i][1]}<br>`)
-      }
-  })
-//   return finalArr
-//   console.log(finalArr);
-//   html=`${finalArr[0]} : ${finalArr[1]}`
-}
-// hein()
-
-// const insertBordel = () => {
-//   return hein()
-// // console.log(hein());
-// }
-// console.log(insertBordel());
-
-
-// fetch("https://www.themealdb.com/api/json/v1/1/random.php").then((res) =>
-//   console.log(res)
-// );
-const jsp = (data) => {
-    let measures = ""
-    let arr1 = []
-    let arr2 = []
-    let finalArr = []
-    arr1 = Object.entries(data.meals[0]).filter((e) => e[0].includes("strIngredient"));
-    arr2 = Object.entries(data.meals[0]).filter((e) => e[0].includes("strMeasure"));
-    for(let i = 0; i < arr1.length; i++){
-        finalArr.push([arr1[i][1], arr2[i][1]])
-    }
-    // console.log(finalArr.filter(e=> e[0]));
-    finalArr = finalArr.filter(e=> e[0])
-    //   finalArr.forEach(e => console.log())
-      for(let i = 0; i < finalArr.length; i++){
-        // ingredients.insertAdjacentHTML("afterbegin", `${finalArr[i][0]} : ${finalArr[i][1]}<br>`)
-        measures += `${finalArr[i][0]} : ${finalArr[i][1]}<br>`
-    }
-
-  const html = `<div>
-  <h2>${data.meals[0].strMeal}</h2>
-  <div class="main">
-  <div class="picAndMeasures">
-  <img src="${data.meals[0].strMealThumb}" alt="tg">
-  <div class="recipe">
-  <div class="wot">${data.meals[0].strInstructions}</div>
-  </div>
-  </div>
-  <div class="grosBordel">${measures}</div>
-  <div>${data.meals[0].strArea}</div>
-    <a href="${data.meals[0].strYoutube}">click here to get an ad</a>
-    </div>
-    </div>`;
-    
-  info.insertAdjacentHTML("afterbegin", html);
-};
-
-req().then((res) => jsp(res));
-
-// const a = () => {
+// const hein = () => {
+//   let arr1 = [];
+//   let arr2 = [];
+//   let finalArr = [];
 //   req().then((res) => {
-//     for (let i = 0; i < 20; i++) {
-//       if (res.meals[0].strIngredient`${i}`) {
-//         console.log(1);
-//       }
+//     arr1 = Object.entries(res.meals[0]).filter((e) =>
+//       e[0].includes("strIngredient")
+//     );
+//     arr2 = Object.entries(res.meals[0]).filter((e) =>
+//       e[0].includes("strMeasure")
+//     );
+//     for (let i = 0; i < arr1.length; i++) {
+//       finalArr.push([arr1[i][1], arr2[i][1]]);
+//     }
+//     // console.log(finalArr.filter(e=> e[0]));
+//     finalArr = finalArr.filter((e) => e[0]);
+//     //   finalArr.forEach(e => console.log())
+//     for (let i = 0; i < finalArr.length; i++) {
+//       ingredients.insertAdjacentHTML(
+//         "afterbegin",
+//         `${finalArr[i][0]} : ${finalArr[i][1]}<br>`
+//       );
 //     }
 //   });
+
 // };
 
-// a()
+const randDish = (data) => {
+  let measures = "";
+  let finalArr = [];
 
-groBtn.addEventListener("click", ()=>{
-    info.innerHTML=""
-    req().then((res) => {
-        jsp(res);
-   
-    
-    });
-})
+  let arr1 = Object.entries(data.meals[0]).filter((e) =>
+    e[0].includes("strIngredient")
+  );
+
+  let arr2 = Object.entries(data.meals[0]).filter((e) =>
+    e[0].includes("strMeasure")
+  );
+
+  for (let i = 0; i < arr1.length; i++) {
+    finalArr.push([arr1[i][1], arr2[i][1]]);
+  }
+  finalArr = finalArr.filter((e) => e[0]);
+  for (let i = 0; i < finalArr.length; i++) {
+    measures += `${finalArr[i][0]} : ${finalArr[i][1]}<br>`;
+  }
+
+  const html = `
+  <h1 class="card" id="title">In need for a dish idea ?</h1>
+  <h2 class="card" id="name">How about a <em>${data.meals[0].strMeal}</em> ?</h2>
+  <div class="card" id="nation"> ${data.meals[0].strArea} meal incoming !<br></div>
+  <a class="card" target="blank" href="${data.meals[0].strYoutube}">click here to see an available videoguide</a>
+  <img class="card" src="${data.meals[0].strMealThumb}" alt="">
+  <div class="card" id="instructions">${data.meals[0].strInstructions}<br><B>&darr;Ingredients Below&darr;</B></div>
+  <div class="card">You are going to need these<br>&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;<br>${measures}</div>
+  `;
+  grid.insertAdjacentHTML("beforeend", html);
+};
+
+request().then((data) => randDish(data));
+
+groBtn.addEventListener("click", () => {
+  grid.innerHTML = "";
+  request().then((data) => {
+    randDish(data);
+  });
+});
