aa568570ec63272e3ccd414882d14e148d1c6557
Gabriel M <gmoreno9109@gmail.com>
2023-10-30 17:23:00 +0100

    responsive

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 
 const groBtn = document.querySelector("button");
 const grid = document.querySelector(".grid");
+const grid2 = document.querySelector(".grid2");
+const grid3 = document.querySelector(".grid3");
+const videoPlayer = document.querySelector(".videoPlayer")
 
 
 const request = async () => {
   const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
   const data = await res.json();
+  console.log(data);
   return data;
 };
 
@@ -56,19 +60,35 @@ const randDish = (data) => {
   for (let i = 0; i < finalArr.length; i++) {
     measures += `${finalArr[i][0]} : ${finalArr[i][1]}<br>`;
   }
-
+console.log(data.meals[0].strYoutube.slice(32));
   const html = `
   <h1 class="card" id="title">In need for a dish idea ?</h1>
-  <h2 class="card" id="name">How about a <em>${data.meals[0].strMeal}</em> ?</h2>
-  <div class="card" id="nation"> ${data.meals[0].strArea} meal incoming !<br></div>
-  <a class="card" target="blank" href="${data.meals[0].strYoutube}">click here to see an available videoguide</a>
-  <img class="card" src="${data.meals[0].strMealThumb}" alt="">
-  <div class="card" id="instructions">${data.meals[0].strInstructions}<br><B>&darr;Ingredients Below&darr;</B></div>
-  <div class="card">You are going to need these<br>&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;&darr;<br>${measures}</div>
+  <h2 class="card" id="name">How about a <em>${data.meals[0].strMeal}</em> ?<br><p>Don't know where to start ?<br>&darr;A videoguide awaits you below&darr;</p></h2>
   `;
+
+  const htmlVideo = `
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/${data.meals[0].strYoutube.slice(32)}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  `
+
+  const html2 =`
+  <div class="card" id="nation"> ${data.meals[0].strArea} meal incoming !<br></div>
+  <div class="picAndIngredients">
+  <img class="card" src="${data.meals[0].strMealThumb}" alt="">
+  <div class="card" id="ingredients"><em>Ingredients</em><br>${measures}</div>
+  </div>
+  `
+  const html3 = `
+  <div class="card" id="instructions">${data.meals[0].strInstructions}</div>
+  
+  `
+
   grid.insertAdjacentHTML("beforeend", html);
+  videoPlayer.insertAdjacentHTML("beforeend", htmlVideo)
+  grid2.insertAdjacentHTML("beforeend", html2);
+  grid3.insertAdjacentHTML("beforeend", html3);
 };
 
+//<a class="card" target="blank" href="${data.meals[0].strYoutube}">click here to see an available videoguide</a>
 request().then((data) => randDish(data));
 
 groBtn.addEventListener("click", () => {
